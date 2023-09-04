470a2f34a2bbff6351475abdd68c6fec6e8e2599
Alberto Iong <albert0i@hotmail.com>
2023-09-04 16:32:39 +0800

    add script6.js

--- script6.js
+++ script6.js
@@ -1,11 +1,25 @@
 //import prisma from "@/lib/prisma"
 import prisma from './lib/prisma.js'
 
-prisma.post.findMany({ 
-        include: {
-            comments: true,
-        }
-    })
-    .then(data => {
-        data.map( rec => console.log('rec=', rec))
-    })
+// prisma.post.findMany({ 
+//         include: {
+//             comments: true,
+//         }
+//     })
+//     .then(data => {
+//         data.map( rec => console.log('rec=', rec))
+//     })
+
+prisma.post.findUnique({ 
+                where: { id: 1}
+            })
+            .then(rec => { 
+                console.log('rec=', rec)
+
+                prisma.post.update({
+                    where: {id: 1},
+                    data: { views: rec.views + 1,
+                            likes: rec.likes + 1 }
+                })
+                .then( res => console.log('res=', res))
+            } )
