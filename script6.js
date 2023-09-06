c82a75533305886a1767f85b36a750fe093f043b
Alberto Iong <albert0i@hotmail.com>
2023-09-06 10:54:23 +0800

    add show sql

--- script6.js
+++ script6.js
@@ -11,15 +11,15 @@ import prisma from './lib/prisma.js'
 //     })
 
 prisma.post.findUnique({ 
-                where: { id: 1}
+                where: { id: 1 }
             })
             .then(rec => { 
-                console.log('rec=', rec)
+                // console.log('old rec=', rec)
 
                 prisma.post.update({
-                    where: {id: 1},
+                    where: { id: 1 },
                     data: { views: rec.views + 1,
                             likes: rec.likes + 1 }
                 })
-                .then( res => console.log('res=', res))
+                .then( res => console.log('new res=', res))
             } )
